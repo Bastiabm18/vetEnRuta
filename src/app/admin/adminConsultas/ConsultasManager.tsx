853a4ebfd0ec1506e21b09126046cf6e011081e2
Bastian Barrios <andresbarriosmedina1@gmail.com>
2025-07-30 18:12:20 -0400

    AJUSTE V.8

--- src/app/admin/adminConsultas/ConsultasManager.tsx
+++ src/app/admin/adminConsultas/ConsultasManager.tsx
@@ -403,6 +403,19 @@ export default function ConsultasManager() {
                             <div className="ml-6 mt-2 w-full border-l pl-4 border-gray-200">
                               <p className="text-md font-semibold text-gray-800 mb-1">Servicios para {mascota.nombre}:</p>
                               <ul className="list-disc list-inside text-sm space-y-1">
+                {cita.precio_base !== undefined && (
+                  <li key="pv1" className="text-gray-600 flex items-center justify-between">
+                        <span className='flex flex-row items-center'>
+                        <FaPaw className="mr-2 text-teal-500 text-xs"/>
+                        Precio visita: ${cita.precio_base.toLocaleString('es-CL')}
+                        </span>
+                      <span className="text-gray-900">
+                     Valor veterinario: ${cita.precio_base_vet.toLocaleString('es-CL')}
+                      </span>
+                    
+                  </li>
+                )}
+
                                 {mascota.servicios.map((servicio, svcIndex) => (
                                   <li key={svcIndex} className="text-gray-600 flex items-center justify-between">
                                     <span className='flex flex-row items-center'><FaPaw className="mr-2 text-teal-500 text-xs" />
@@ -455,14 +468,7 @@ export default function ConsultasManager() {
                     </p>
                   </div>
                 )}
-                {cita.precio_base !== undefined && (
-                  <div className="p-4 rounded-md bg-gray-50 border border-gray-200 mb-4">
-                    <p className="text-base font-semibold text-gray-700 flex justify-between items-center">
-                      <span>Precio visita:</span>
-                      <span className="text-gray-900">${cita.precio_base.toLocaleString('es-CL')}</span>
-                    </p>
-                  </div>
-                )}
+
                 {/* Sección para el Monto Total */}
                 { cita.montoTotal !== undefined && (
                   <div className="bg-indigo-50 p-4 rounded-md border-t border-b border-indigo-200 mt-4">
